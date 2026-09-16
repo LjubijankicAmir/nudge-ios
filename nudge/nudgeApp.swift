f3d4eac23ecb235839e57d174cd1bf3ff88ee75b
Amir Ljubijankic <amir.ljubijankic1@stu.ibu.edu.ba>
2026-09-16 15:44:58 +0200

    Initial Commit

---
+++ nudge/nudgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nudgeApp.swift
+//  nudge
+//
+//  Created by User on 16. 9. 2026..
+//
+
+import SwiftUI
+
+@main
+struct nudgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ nudgeTests/nudgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  nudgeTests.swift
+//  nudgeTests
+//
+//  Created by User on 16. 9. 2026..
+//
+
+import Testing
+@testable import nudge
+
+struct nudgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
